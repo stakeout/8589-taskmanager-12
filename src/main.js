dd7869e7acd2e26c830c032de543dd93f811eb04
Vadim Novash <novash@tut.by>
2020-08-05 17:23:20 +0300

    create html task template
    - create html task tpl
    - render task template with some mocks data

--- src/main.js
+++ src/main.js
@@ -2,29 +2,30 @@ import {renderHtmlElement} from './utils';
 import {createMenuTemplate as menu} from './view/site-menu';
 import {createTaskFiltersTemplate as filter} from './view/filter';
 import {createCardsSortTemplate as sort} from './view/tasks-sort';
-import {createTaskFormTemplate as form} from './view/task-edit';
-import {createTaskCardTemplate as task} from './view/task';
+// import {createTaskFormTemplate as form} from './view/task-edit';
+// import {createTaskCardTemplate as task} from './view/task';
 import {createLoadButtonTemplate as loadMoreBtn} from './view/load-more-btn';
-import {generateTask} from "./mock/task.js";
+import {generateTask, createTaskTemplate} from "./mock/task.js";
 
-const TASK_AMOUNT = 3;
-const TASK_COUNT = 5;
+const TASK_AMOUNT = 10;
 const content = document.querySelector(`.main`);
 const control = content.querySelector(`.control`);
 const boardContainer = content.querySelector(`.board`);
 const taskListContainer = boardContainer.querySelector(`.board__tasks`);
 
-const tasks = new Array(TASK_COUNT).fill().map(generateTask);
+const tasks = new Array(TASK_AMOUNT).fill().map(generateTask);
+console.log(tasks);
 
 const addTasks = (container, position) => {
-  for (let i = 0; i < TASK_AMOUNT; i += 1) {
-    renderHtmlElement(container, task(), position);
+  const length = tasks.length - 1;
+  for (let i = 0; i <= length; i += 1) {
+    renderHtmlElement(container, createTaskTemplate(tasks[i]), position);
   }
 };
 
 renderHtmlElement(control, menu(), `beforeend`);
 renderHtmlElement(control, filter(), `afterend`);
 renderHtmlElement(boardContainer, sort(), `afterbegin`);
-renderHtmlElement(taskListContainer, form(), `beforeend`);
+// renderHtmlElement(taskListContainer, form(), `beforeend`);
 addTasks(taskListContainer, `beforeend`);
 renderHtmlElement(boardContainer, loadMoreBtn(), `beforeend`);
