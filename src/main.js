f33808e914f0d5472b1eab710367d288b9096ee9
Vadim Novash <novash@tut.by>
2020-08-20 21:16:29 +0300

    add task-edit class component
    - remove taskTemplate from task mock

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
-import {renderTemplate, renderElement, RenderPosition} from './utils.js';
-import SiteMenu from './view/site-menu.js';
+import {renderElement, RenderPosition} from './utils.js';
+import SiteMenuView from './view/site-menu.js';
 import FilterView from './view/filter.js';
 import SortView from './view/tasks-sort.js';
 import BoardView from './view/board.js';
 import TaskListView from './view/task-list.js';
 import TaskView from './view/task.js';
 import LoadMoreButtonView from './view/load-more-btn.js';
-import {createTaskFormTemplate} from "./view/task-edit.js";
-import {generateTask, createTaskTemplate} from "./mock/task.js";
+import TaskEditView from "./view/task-edit.js";
+import {generateTask} from "./mock/task.js";
 import {generateFilter} from "./mock/filter.js";
 
 const TASK_AMOUNT = 20;
 const TASK_COUNT_PER_STEP = 8;
+
 const siteMainElement = document.querySelector(`.main`);
 const control = siteMainElement.querySelector(`.control`);
+
 const boardComponent = new BoardView();
 
 const tasks = new Array(TASK_AMOUNT).fill().map(generateTask);
+console.log(tasks)
 const filters = generateFilter(tasks);
 
 const addTasks = (container, position) => {
@@ -25,13 +28,13 @@ const addTasks = (container, position) => {
   }
 };
 
-renderElement(control, new SiteMenu().getElement(), RenderPosition.BEFOREEND);
+renderElement(control, new SiteMenuView().getElement(), RenderPosition.BEFOREEND);
 renderElement(siteMainElement, new FilterView(filters).getElement(), RenderPosition.BEFOREEND);
 renderElement(siteMainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
 renderElement(boardComponent.getElement(), new SortView().getElement(), RenderPosition.BEFOREEND);
 const taskListComponent = new TaskListView();
 renderElement(boardComponent.getElement(), taskListComponent.getElement(), RenderPosition.BEFOREEND);
-// renderTemplate(taskListContainer, createTaskFormTemplate(tasks[0]), `afterbegin`);
+renderElement(taskListComponent.getElement(), new TaskEditView(tasks[0]).getElement(), RenderPosition.BEFOREEND);
 addTasks(taskListComponent.getElement(), RenderPosition.BEFOREEND);
 
 
