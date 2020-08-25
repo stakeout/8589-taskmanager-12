b82c85574d08ff884c6f4a0399906e26f199f25b
Vadim Novash <novash@tut.by>
2020-08-25 13:43:16 +0300

    update simple classes, remove common methods

--- src/view/load-more-btn.js
+++ src/view/load-more-btn.js
@@ -1,28 +1,13 @@
 
-import {createElement} from "../utils.js";
+import AbstractView from './abstract';
 
 const createLoadButtonTemplate = () => {
   return `<button class="load-more" type="button">load more</button>`;
 };
 
-export default class LoadMoreButton {
-  constructor() {
-    this._element = null;
-  }
+export default class LoadMoreButton extends AbstractView {
 
   getTemplate() {
     return createLoadButtonTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
